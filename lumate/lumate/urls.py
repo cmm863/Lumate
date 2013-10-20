c6854d8d0b3586c349ce9f233e2d950a272ff55a
Ubuntu <ubuntu@ip-172-31-42-153.us-west-2.compute.internal>
2013-10-20 07:03:43 +0000

    Final touch-ups, clean the html and disable debugging so proper 404's are found

--- lumate/lumate/urls.py
+++ lumate/lumate/urls.py
@@ -6,7 +6,7 @@ from django.conf.urls import patterns, include, url
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'lumate.views.home', name='home'),
+    url(r'^$', 'lumate.views.index'),
     # url(r'^lumate/', include('lumate.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
---
+++ lumate/lumate/views.py
@@ -0,0 +1,7 @@
+from django.http import HttpResponse
+from django.http import HttpResponseRedirect
+
+
+def index(request):
+    return HttpResponseRedirect('guestbook/create/')
+
